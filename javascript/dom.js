c952b19694075b47350e9be91a0c027e68f1d9a5
Himavadan Kamera <himavadhan88@gmail.com>
2025-06-25 15:12:22 +0530

    basic js

---
+++ javascript/dom.js
@@ -0,0 +1,11 @@
+document.body.style.backgroundColor="yellow";
+document.title= "DOM IN JavaScript";
+document.getElementById("header")
+.innerHTML="hello";
+document.getElementById("para").style.color="red";
+document.getElementById("para").style.fontSize="50px";
+document.getElementById("Aiml").style.color="darkgreen";
+document.getElementById("Cse").style.fontSize="30px";
+document.getElementById("It").style.fontFamily="arial";
+document.getElementsByClassName("a")[0].style.color="red";
+document.getElementsByClassName("a")[0].style.fontSize="50px";
